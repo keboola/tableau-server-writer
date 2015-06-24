530bbf11b9e57032ed19d66cea9f9fce4c5bc8a9
Jakub Matejka <jakub@keboola.com>
2015-06-24 16:31:23 +0200

    init

---
+++ src/Keboola/TableauServerWriter/Exception.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jakub
+ * Date: 24/06/15
+ * Time: 16:19
+ */
+
+namespace Keboola\TableauServerWriter;
+
+class Exception extends \Exception
+{
+
+}
---
+++ src/Keboola/TableauServerWriter/Writer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jakub
+ * Date: 24/06/15
+ * Time: 16:17
+ */
+
+namespace Keboola\TableauServerWriter;
+
+
+class Writer
+{
+
+}
